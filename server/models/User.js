a4f8ba6efbdf7debe8702301ae5363c4715feb2c
guitarkeegan <keegananglim@gmail.com>
2022-08-02 16:17:56 -0700

    change orderdetails in order model

--- server/models/User.js
+++ server/models/User.js
@@ -1,37 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
-const orderSchema = require('./Order');
+const bcrypt = require("bcrypt");
+const orderSchema = require("./Order");
 
-let validateEmail = function(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
+let validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
 };
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    max_length: 50,
-    trim: true,
-    lowercase: true, 
-    validate: [validateEmail, 'Please fill a valid email address'],
-    match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address'] // not sure why I had both regexs here...
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      max_length: 50,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Please fill a valid email address",
+      ], // not sure why I had both regexs here...
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    orders: [orderSchema],
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  orders: [orderSchema] // Order or Order.schema
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // set up pre-save middleware to create password
-userSchema.pre('save', async function(next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -40,10 +52,10 @@ userSchema.pre('save', async function(next) {
 });
 
 // compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
