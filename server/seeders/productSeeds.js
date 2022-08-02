810f14ca49ca30c3b067357ddba52d53f5396a27
guitarkeegan <keegananglim@gmail.com>
2022-08-02 16:30:12 -0700

    change instock to stockquantity

--- server/seeders/productSeeds.js
+++ server/seeders/productSeeds.js
@@ -6,7 +6,7 @@ const products = [
     "description": "Delightfully smooth Black tea with accentuated flavors of bergamot oil.",
     "name": "Vahdam Earl Grey Citrus Black Tea",
     "price": 23.99,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -14,7 +14,7 @@ const products = [
     "description": "Smooth, sensual and rich, Tielka's best-kept secret and a unique take on a French Earl Grey.",
     "name": "Tielka Lady Betty Black Tea",
     "price": 25.00,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -22,7 +22,7 @@ const products = [
     "description": "A unique blend of black tea with notes of fruits and flowers from the Bermuda triangle which leaves a lingering aftertaste of ripe berries, anise, and caramel.",
     "name": "TWG Silver Moon",
     "price": 32.99,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -30,7 +30,7 @@ const products = [
     "description": "Complex, full-bodied, lively cup of tea that is perfect any time of day.",
     "name": "Twining's English Breakfast",
     "price": 11.42,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -38,7 +38,7 @@ const products = [
     "description": "A tea of the month club that curates amazing single origin teas from around the world. Nepal, Sri Lanka, Kenya, & beyond.",
     "name": "Atlas Tea Club",
     "price": 14.00,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -46,7 +46,7 @@ const products = [
     "description": "Collection of Tea Forte infusions, with two signature pyramid infusers each of our most popular black tea, herbal tea, oolong tea, green tea and white tea blends.",
     "name": "Tea Forté Sampler",
     "price": 66.00,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -54,7 +54,7 @@ const products = [
     "description": "A silky, full body blend of black tea with hints of citrus for a complex and soothing flavor.",
     "name": "Art of Tea Earl Grey Crème",
     "price": 21.00,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -62,7 +62,7 @@ const products = [
     "description": "Hot Cinnamon Spice is an assertive blend of black teas, three types of cinnamon, orange peel, and sweet cloves. No sugar added.",
     "name": "Harney & Sons Hot Cinnamon Spice",
     "price": 11.60,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -70,7 +70,7 @@ const products = [
     "description": "Emperors had this tea plucked from their secret gardens to share with visiting dignitaries who reveled in the airy fragrance and pure flavor.",
     "name": "Republic of Tea White Emperor",
     "price": 15.99,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -78,7 +78,7 @@ const products = [
     "description": "Captures the delicate, refreshing, mouth-tingling flavor of peppermint. Naturally caffeine-free, its strong menthol flavor will invigorate your mouth.",
     "name": "Stash Peppermint Herbal Tea",
     "price": 17.94,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -86,7 +86,7 @@ const products = [
     "description": "In this classic Royal Blend notes of Flowery Pekoe from Sri Lanka uplift the maltier Assam to create a very traditional cup of tea.",
     "name": "Fortnum & Mason Royal Blend",
     "price": 12.10,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -94,7 +94,7 @@ const products = [
     "description": "Chinese green tea with roses, green dates, berries and orange flower water.",
     "name": "Palais des Thes Hammam Tea",
     "price": 14.50,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -102,7 +102,7 @@ const products = [
     "description": "Organic Ceremonial Grade Matcha, suited for traditional Matcha Tea preparation where Matcha is simply whisked with hot water.",
     "name": "Jade Leaf Ceremonial Matcha",
     "price": 19.95,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -110,7 +110,7 @@ const products = [
     "description": "Floral Chamomile and Rooibos with notes of sweet Caramel and Vanilla Flavor for a delicious and warming herbal tea.",
     "name": "Yogi Soothing Caramel Bedtime",
     "price": 24.84,
-    "instock": 10
+    "stockQuantity": 10
 },
 {
     
@@ -118,7 +118,7 @@ const products = [
     "description": "Variety packs include six individually wrapped and tagged tea bags of each flavor.",
     "name": "Taylors of Harrogate Assorted Specialty Teas",
     "price": 11.49,
-    "instock": 10
+    "stockQuantity": 10
 }
 ]
 
