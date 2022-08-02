31db9f4421232d50d7deba4ff8bad8b1ff6bdf32
simmy varghese <simmyvarghese5@gmail.com>
2022-08-02 16:02:57 -0700

    Updated the auth file-Removed name field,Added me query in resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,23 @@
-const {User,Product,Order}=require('../models');
+const {User,Product}=require('../models');
 const { AuthenticationError } = require('apollo-server-express');
-const { User,Order,Product } = require('../models');
 const { signToken } = require('../utils/auth');
 
 
+const resolvers={
+
+Query:{    
 //Get user data
+    me:async(parent,args,context)=>{
+        if(context.user){
+
+            const userData=User.findOne({ _id: context.user._id }).select('-__v -password')
+            return userData;
+        }
+
+        throw new AuthenticationError('User not logged in');
+
+    }
+
 
 
 
@@ -18,5 +31,8 @@ const { signToken } = require('../utils/auth');
 
 //Get all Order specific to the user logged in
 
+}
+}
 
 
+module.exports=resolvers;
