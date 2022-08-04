d0491a8944b4cbcb939577affa4ad5420891bbdc
simmy varghese <simmyvarghese5@gmail.com>
2022-08-04 16:28:58 -0700

    Fixed Missing Links Issue

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,11 +1,10 @@
 
-
 function Card(props){
    
   return(
     <div className="card">
       <div className="cardBody">
-        <img className="cardImage" src={require ( `../../images/${props.product.image}`)} alt=""/>
+        <img className="cardImage" src={require( `../../images/${props.product.image}`)} alt=""/>
         <h2 className="cardTitle">{props.product.name}</h2>
         <p className="cardDescription">{props.product.description}</p>
         <h3 className="cardPrice">${props.product.price}</h3>
