2a88aa5bb9d99852572409377033e6d27135c685
DWKanas <dkanas0122@gmail.com>
2022-08-04 13:42:32 -0700

    fixed image and cards

---
+++ client/src/images/index.js
@@ -0,0 +1,19 @@
+const IMAGES = {
+    artoftea: require('./ArtofTeaEarlGreyCreme.jpg').default,
+    atlastea: require('./AtlasTeaClub.jpg').default,
+    fortnum: require('./Fortnum&MasonRoyalBlend.jpg').default,
+    harney: require('./Harney&SonsHotCinnamonSpice.jpg').default,
+    jade: require('./JadeLeafCeremonialMatcha.jpg').default,
+    palaisdes: require('./PalaisdesThesHammamTea.jpg').default,
+    republic: require('./RepublicofTeaWhiteEmperor.jpg').default,
+    stash: require('./StashPeppermintHerbalTea.jpg').default,
+    taylors: require('./TaylorsofHarrogateAssortedSpecialtyTeas.jpg').default,
+    teaforte: require('./TeaForteSampler.jpg').default,
+    tielka: require('./TielkaLadyBettyBlackTea.jpg').default,
+    twg: require('./TWGSilverMoon.jpg').default,
+    twinings: require('./TwiningsEnglishBreakfast.jpg').default,
+    vahdam: require('./VahdamEarlGreyCitrusBlackTea.png').default,
+    yogi: require('./YogiSoothingCaramelBedtime.jpg').default,
+}
+
+export default IMAGES;
