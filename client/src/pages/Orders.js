478b389d07f8d29663c6cf934beae9e4c77b30f1
simmy varghese <simmyvarghese5@gmail.com>
2022-08-06 12:40:12 -0700

    Added logic to display products fro each array with its details and quantity

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -1,100 +1,100 @@
 import React,{useState} from "react";
 import { useQuery } from "@apollo/client";
-import { QUERY_ME, QUERY_PRODUCTS } from "../utils/queries";
-// useing query to render products
+import { QUERY_ME} from "../utils/queries";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Calculate count for products in each order
+function calculateCount(productArray,count){
+  
+ for(let product of productArray){
+  if(count[product._id])
+{count[product._id]+=1;}
+else{
+  count[product._id]=1;
+}
+
+ }
+  
+
+
+}
+
 const Orders = () => {
 
   const {loading,data} = useQuery(QUERY_ME);
   
-  const count={};
-  const productIds=[];
+  //To hold the quantity of products in each order
+  let count={};
 
-  console.log("Orders Page");
+  //To hold the productId's to check of there is repeated products in the same order 
+  let productIds=[];
 
-  // console.log(data?.me);
 
   const orders=data?.me.orders || []
 
   const products=orders.productDetails
 
-  // console.log(orders);
-
+  
+//If Order array is empty return h3 element
   if (!orders.length) {
     return <h3>No Order Available</h3>;
   }
 
-  //Calculate the Quantity of a product in every order
- for(const eachOrder of orders){
-    for(const product of eachOrder.productDetails){
-     
-      if(count[product._id]){
-
-        count[product._id]+=1;
-      }
-      else{
-        count[product._id]=1;
-      }
-
-
-    }
-
-
- }
- var duplicateFlag=false;
-
-  console.log("Count",{...count});
-
-  // console.log(...orders);
 
   return (
     <main>
       <div className="row">
         {
-        orders.map((order)=>
+        orders.map((order,index)=>{
 
-          <div className="card">
-            <div className="card-header d-flex justify-content-between px-2">
-              <div>Order No #</div>
-              <div>{order.createdAt}</div>
-            </div>
-            
+          //Reset count and productId for each order
+          count={};
+          productIds=[];
 
-          <ul className="list-group list-group-flush">
-
-           
-        
-          
-
-          { order.productDetails.map((product,index,array)=>
-             
-         {
-          console.log(productIds);
-          console.log(productIds.indexOf(product._id));
-          console.log(product._id);
-            if(productIds.indexOf(product._id)==-1){
-              productIds.push(product._id)
-            //To Do To display the quantity for repeated entries
-            //Test by adding multiple products into the page
-           return( <li key={product._id} className="list-group-item order-productlist d-flex justify-content-between">
-              <img src={require( `../images/${product.image}`)}/>
-              <p className="product-name">{product.name}</p>
-              <p className="product-price">{product.price}</p>
-              <p className="product-quantity">{count[product._id]}</p>
-            </li>)
-            
-            }
-           
-          
+         //If order gets inserted with no products
+          if(order.productDetails.length===0){
+             return <p>No products Added to this Order</p>
           }
-           )
-           
-           }
-           </ul>
+       
+        else{
+    
+    return (
+            <div className="card">
+                <div className=" d-flex justify-content-between align-items-center p-2">
+                  <div>Order No {index+1}</div>
+                  <div>Total Price {order.totalPrice}</div>
+                  <div>{order.createdAt}</div>
+                </div>
 
-          </div>
-        )
-        }
+                <ul className="list-group list-group-flush">
+                <div className=" d-flex justify-content-between align-items-center p-2">
+                  <div>Image</div>
+                  <div>Product</div>
+                  <div>Price</div>
+                  <div>Qty</div>
+                </div>
+                <hr/>
+                  {/* Loop  through each product per order,calculate quantity as count and display only unique products */}
+                  { order.productDetails.map((product,index,array)=>
+                  {
+                    calculateCount(array,count);
+                       
+                  if(productIds.indexOf(product._id)==-1){
+                    productIds.push(product._id)
+                    
+                    return( <li key={product._id} className="list-group-item order-productlist d-flex justify-content-between  align-items-center">
+                            <img src={require( `../images/${product.image}`)}/>
+                            <p className="product-name">{product.name}</p>
+                            <p className="product-price">{product.price}</p>
+                             <p className="product-quantity">{count[product._id]}</p>
+                            </li>)
+          }
+          })}
+          </ul>
+    </div>)
+
+}})
+}
       </div>
     </main>
   );
