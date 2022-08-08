e69813def3b79ecc654c84b1a2626fc32f483210
guitarkeegan <keegananglim@gmail.com>
2022-08-07 23:33:51 -0700

    add payment compentents to payment page

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -1,11 +1,26 @@
+import React, { useState } from "react";
+import {PaymentForm, ShippingForm} from "../components/Payment";
 
+const Payment = () => {
 
-const Payment=()=>{
+    const styles = {
+        formDiv: {
+            maxWidth: 400,
+        }
+    }
 
-
-    return <div>Payment Page</div>
-
-
-}
+  return (
+    <div className="row justify-content-evenly">
+    <div style={styles.formDiv} className="col-12 col-6-md">
+    <h2>Payment</h2>
+      <PaymentForm />
+    </div>
+    <div style={styles.formDiv} className="col-12 col-6-md">
+    <h2>Shipping</h2>
+      <ShippingForm />
+    </div>
+    </div>
+  );
+};
 
 export default Payment;
