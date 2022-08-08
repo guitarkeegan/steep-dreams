ef3418d1add56e5b23966e1c07b20f976c857bd6
James <james91055@gmail.com>
2022-08-08 14:51:46 -0700

    update home picture

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -21,29 +21,33 @@ const Home = () => {
         )}
        
       </div> */}
+      <div className="row m-5 justify-content-center align-item-center col-11">
+       
+          <ul className="row text-center ">
 
-      <div className="m-5 ">
-        <ul className="home-card-text d-flex text-center">
-          <Link className="" to="/about">
-            <li className="container click" href="#about">
-              <h2 className="">Products</h2>
-              <img
-                className="home-card "
-                src="https://images.pexels.com/photos/6713241/pexels-photo-6713241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              ></img>
-            </li>
-          </Link>
-          <Link className="" to="/Products">
-            <li className="container click">
-              <h2 className="">Products</h2>
-              <img
-                className="home-card"
-                src="https://images.pexels.com/photos/6713241/pexels-photo-6713241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              ></img>
-            </li>
-          </Link>
-        </ul>
-      </div>
+            <Link className="homelist col-md-6 col-10" to="/about">
+              <li className="container click mb-3 " href="#about">
+                <h2 className="home-card-text">About Us</h2>
+                <img 
+                  className="home-card img-fluid "
+                  src="https://images.pexels.com/photos/3971929/pexels-photo-3971929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                ></img>
+              </li>
+            </Link>
+
+            <Link className="homelist col-md-6 col-10" to="/Products">
+              <li className="container click">
+                <h2 className="home-card-text">Products</h2>
+                <img
+                  className="home-card img-fluid"
+                  src="https://images.pexels.com/photos/6465733/pexels-photo-6465733.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                ></img>
+              </li>
+            </Link>
+
+          </ul>
+        </div>
+      
     </main>
   );
 };
