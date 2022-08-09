069103bcbfb129fed019b1fede11ddec2b643b58
guitarkeegan <keegananglim@gmail.com>
2022-08-09 01:29:48 -0700

    fix spelling

--- client/src/components/Payment/index.js
+++ client/src/components/Payment/index.js
@@ -150,7 +150,7 @@ export function ShippingForm({handleSubmit}) {
       </Row>
 
       <Form.Group className="mb-3">
-        <Form.Check type="checkbox" label="Click to recieve order updates" />
+        <Form.Check type="checkbox" label="Click to receive order updates" />
       </Form.Group>
 
 
