bae36942980b25fd8bbade059d6991448d892947
guitarkeegan <keegananglim@gmail.com>
2022-08-10 14:18:14 -0700

    home is better but not done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,31 +12,29 @@ const Home = () => {
         className="hero"
         src="https://images.pexels.com/photos/6713241/pexels-photo-6713241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
       ></img>
-      <div className="row m-5 justify-content-center align-item-center col-11">
+      <div className="row text-center mt-3 mb-3">
        
-          <ul className="row text-center ">
-
-            <Link className="homelist col-md-6 col-10" to="/about">
-              <li className="container click mb-3 " href="#about">
+            <Link className="col-12 col-md-6 mt-2 mb-2" to="/about">
+              
                 <h2 className="home-card-text">About Us</h2>
                 <img 
                   className="home-card img-fluid "
                   src="https://images.pexels.com/photos/3971929/pexels-photo-3971929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                 ></img>
-              </li>
+              
             </Link>
 
-            <Link className="homelist col-md-6 col-10" to="/Products">
-              <li className="container click">
+            <Link className="col-12 col-md-6 mt-2 mb-2" to="/Products">
+              
                 <h2 className="home-card-text">Products</h2>
                 <img
                   className="home-card img-fluid"
                   src="https://images.pexels.com/photos/6465733/pexels-photo-6465733.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                 ></img>
-              </li>
+        
             </Link>
 
-          </ul>
+          
         </div>
       
     </main>
