3ffc440fe86003f4502ee226388f96f5b3a8451d
guitarkeegan <keegananglim@gmail.com>
2022-09-13 12:21:50 -0700

    add search reducer

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -18,12 +18,30 @@ function searchProduct(event){
       window.location.assign(`/products/${searchElement.value}`);
     }
 
+const initialState = {
+  loading: false,
+  results: [],
+  value: '',
+};
+
+function searchReducer(state, action){
+  switch (action.type){
+    case 'CLEAN_QUERY':
+      return initialState
+    case 'START_SEARCH':
+      return { ...state, loading: true, value: action.query }
+    case 'FINISH_SEARCH':
+      return { ...state, loading: false, results: action.results}
+    case 'UPDATE_SELECTION':
+      return { ...state, value: action.selection }
+    default:
+      throw new Error()
+  }
+}
+
 const SearchBar=function(){
 
     const [searchState, setSearchState] = useState('');
-    // const [allProducts, setAllProducts] = useState([])
-    
-
 
     const { error, data } = useQuery(QUERY_PRODUCTS);
     const products = data?.getProducts || [];
